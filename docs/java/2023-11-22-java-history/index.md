36e4ebfde0b08edab820cd0c3a2f15b4c1ca4255
Minh Duong <teobatman00@gmail.com>
2023-11-22 18:04:03 +0700

    update new page and config

---
+++ docs/java/2023-11-22-java-history/index.md
@@ -0,0 +1,12 @@
+---
+id: java-history
+title: Lịch sử Java
+tags:
+  - Java
+  - Java History
+slug: /lich-su-java
+---
+
+# Lịch sử Java
+
+Khi bắt đầu tìm hiểu một ngôn ngữ lập trình nào đó, việc tìm hiểu lịch sử về ngôn ngữ đó là điều không thể bỏ qua. 
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -147,7 +147,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} by Minh Dương`,
     },
     prism: {
       theme: prismThemes.github,
