a137d0bbd5728e73956d5e8a5f6234373a196c8e
Minh Duong <teobatman00@gmail.com>
2023-12-20 20:01:39 +0700

    add vn vat law

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,6 +2,8 @@ import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 import "dotenv/config";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const config: Config = {
   title: "Minh Duong",
@@ -32,6 +34,15 @@ const config: Config = {
   customFields: {
     forbiddenGiscusDocPaths: ["/docs/intro"],
   },
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous'
+    }
+  ],
   presets: [
     [
       "classic",
@@ -45,6 +56,10 @@ const config: Config = {
           breadcrumbs: true,
           showLastUpdateTime: true,
           showLastUpdateAuthor: true,
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [
+            [rehypeKatex, {strict: false}]
+          ]
         },
         googleTagManager: {
           containerId: "G-0DNVWKFWGV",
@@ -90,6 +105,10 @@ const config: Config = {
         blogTitle: "✏ Bài viết mới nhất ✏",
         blogDescription: "Danh sách bài viết mới nhất",
         showReadingTime: true,
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [
+          [rehypeKatex, {strict: false}]
+        ]
       },
     ],
     [
